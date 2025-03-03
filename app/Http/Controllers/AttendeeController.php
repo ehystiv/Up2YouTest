39f4137d09e8cafb57f62506429d378718828440
Stefano Bichicchi <sbichicchi@alteanet.it>
2025-03-03 14:26:41 +0100

    ✨ feat(app): Add resource Attendee & relative

---
+++ app/Http/Controllers/AttendeeController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAttendeeRequest;
+use App\Http\Requests\UpdateAttendeeRequest;
+use App\Models\Attendee;
+use Symfony\Component\HttpFoundation\Response;
+
+class AttendeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Attendee::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAttendeeRequest $request)
+    {
+        $attendee = new Attendee();
+
+        $attendee->fill($request->validated());
+        $attendee->save();
+
+        return response()->json($attendee, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Attendee $attendee)
+    {
+        return $attendee;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAttendeeRequest $request, Attendee $attendee)
+    {
+        $attendee->fill($request->validated());
+        $attendee->save();
+
+        return response()->json($attendee, Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Attendee $attendee)
+    {
+        $attendee->delete();
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             EventSeeder::class,
+            AttendeeSeeder::class,
         ]);
     }
 }
